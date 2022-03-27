6d1368252b4e7deb98e0d795577ad33574cfd7d4
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-03-27 09:14:25 +0300

    Update vinaigrette.py
    try_2

--- tar5/vinaigrette.py
+++ tar5/vinaigrette.py
@@ -4,24 +4,33 @@ from datetime import datetime
 import calendar
 
 
-def random_date(start, end):
+def random_date(start_date, end_date):
     """
-    This function will return a random datetime between two datetime
-    objects.
+    This function get two datetime objects: start and end.
+    return a random datetime between two datetime objects.
     """
-    delta = end - start
+    delta = end_date - start_date
     int_delta = (delta.days * 24 * 60 * 60)
     random_second = randrange(int_delta)
-    return start + timedelta()
+    return start + timedelta(seconds=random_second)
 
 
 def if_monday():
-    d1 = datetime.strptime(input("enter date 1: "), '%m-%d-%Y')
-    d2 = datetime.strptime(input("enter date 2: "), '%m-%d-%Y')
-    my_date = random_date(d1, d2)
+    """
+    The function picks up two dates from the user and sends to the function for a random date between them.
+     For a date that is Monday a message will be printed
+    """
+    start_date = datetime.strptime(input("enter date 1: "), '%m-%d-%Y')
+    end_date = datetime.strptime(input("enter date 2: "), '%m-%d-%Y')
+    my_date = random_date(start_date, end_date)
     print(my_date)
     if calendar.day_name[my_date.weekday()] == 'Monday':
         print("I do not have vinaigrette!")
 
 
-if_monday()
+def main():
+    if_monday()
+
+
+if __name__ == "__main__":
+    main()

66058496c2e484d7f829ac7fbccab487058935cd
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-03-27 20:26:29 +0300

    Update vinaigrette.py
    try_4

--- tar5/vinaigrette.py
+++ tar5/vinaigrette.py
@@ -15,17 +15,29 @@ def random_date(start_date, end_date):
     return start + timedelta(seconds=random_second)
 
 
-def if_monday():
+def get_date():
     """
     The function picks up two dates from the user and sends to the function for a random date between them.
-    For a date that is Monday a message will be printed
+    return the random date. 
     """
     start_date = datetime.strptime(input("Enter Date 1: "), '%m-%d-%Y')
     end_date = datetime.strptime(input("Enter Date 2: "), '%m-%d-%Y')
     my_date = random_date(start_date, end_date)
+    return my_date
+
+
+def if_monday():
+    """
+    get a date from get_date function.
+    For a date that is Monday a message will be printed
+    """
+    my_date = get_date()
     print(my_date)
     if calendar.day_name[my_date.weekday()] == 'Monday':
         print("I do not have vinaigrette!")
+        
+
+    
 
 
 def main():

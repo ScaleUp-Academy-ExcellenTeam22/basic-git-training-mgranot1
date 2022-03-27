cb07e049f74b07e32c0a5e24bd51fba85c532d61
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-03-27 09:22:25 +0300

    Update cake.py
    try_2

--- tar5/cake.py
+++ tar5/cake.py
@@ -1,5 +1,11 @@
 
 def get_recipe_price(d, optionals=[], **ingredients):
+     """
+    :param dictionary_ingredients: Dictionary of cake ingredients
+    :param optionals: Ingredients that we will not take for the cake
+    :param ingredients: Ingredients we will take for the cake
+    :return: The cost of preparing the cake
+    """
     sum_all = 0
     for ingredient in ingredients:
         price_100_gr = d[ingredient]
@@ -9,9 +15,15 @@ def get_recipe_price(d, optionals=[], **ingredients):
     return sum_all
 
 
-print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
-# 44
-print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300))
-# 54
-print(get_recipe_price({}))
-#  0
+
+def main():
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
+    # 44
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300))
+    # 54
+    print(get_recipe_price({}))
+    # 0
+
+
+if __name__ == "__main__":
+    main()

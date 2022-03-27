32b46e31ade853f80ef1caf98957903d53db3caf
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-03-27 09:04:45 +0300

    Update cup_of_join.py
    try_2

--- tar5/cup_of_join.py
+++ tar5/cup_of_join.py
@@ -1,4 +1,9 @@
 def join(*lists, sep='-'):
+      """
+    :param lists:
+    :param sep: to be between the lists
+    :return: A new list from a combination of all the lists we received with a mark between them
+    """
     if len(lists) == 0:
         return None
     new_list = []
@@ -8,8 +13,12 @@ def join(*lists, sep='-'):
     return new_list[:-1]
 
 
-print(join(sep='/'))
-print(join([1, 2], [8], [9, 5, 6], sep='@'))
-print(join([1, 2], [8], [9, 5, 6]))
-print(join([1]))
+def main():
+    print(join(sep='/'))
+    print(join([1, 2], [8], [9, 5, 6], sep='@'))
+    print(join([1, 2], [8], [9, 5, 6]))
+    print(join([1]))
 
+
+if __name__ == "__main__":
+    main()
